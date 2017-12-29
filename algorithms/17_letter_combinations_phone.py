2b498285ea08cf40cc4b587431d6f701dfaebfde
Jonathan <jonathan.j.buck@gmail.com>
2017-12-29 13:43:09 -0800

    Solve problem #17.

--- algorithms/17_letter_combinations_phone.py
+++ algorithms/17_letter_combinations_phone.py
@@ -4,3 +4,13 @@ class Solution:
         :type digits: str
         :rtype: List[str]
         """
+        digit_map = {'2':'abc', '3':'def', '4':'ghi', '5':'jkl', '6':'mno','7':'pqrs','8':'tuv','9':'wxyz'}
+        if digits == '':
+            letters = []
+        else:
+            letters = ['']
+        for digit in digits:
+            letters = [x+y for y in digit_map[digit] for x in letters]
+        return letters
+    
+assert Solution().letterCombinations("23") == ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
