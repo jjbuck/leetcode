1e67d7e006beb2c94a32cbf3de864c010bf4a735
Jonathan <jonathan.j.buck@gmail.com>
2017-12-29 16:05:57 -0800

    Solve problem #20.

--- algorithms/20_valid_parentheses.py
+++ algorithms/20_valid_parentheses.py
@@ -4,7 +4,18 @@ class Solution:
         :type s: str
         :rtype: bool
         """
+        char_pair = {')':'(', '}':'{', ']':'['}
+        stack = []
+        for char in s:
+            if char in char_pair.values():
+                stack.append(char)
+            if char in char_pair.keys():
+                if stack == [] or char_pair[char] != stack.pop(): # if stack is empty or if pair for current char is not in stack
+                    return False
+        return stack == [] # only true if stack has been emptied, i.e., all parentheses paired
+
 
 assert Solution().isValid("()") == True
 assert Solution().isValid("()[]{}") == True
 assert Solution().isValid("(]") == False
+assert Solution().isValid("[") == False
