5459fe14ea5a1fdadb0170b478e69b6223adbe9a
Jonathan <jonathan.j.buck@gmail.com>
2018-01-02 21:25:38 -0800

    Start problem #66.

---
+++ algorithms/66_plus_one.py
@@ -0,0 +1,10 @@
+class Solution:
+    def plusOne(self, digits):
+        """
+        Given a non-negative integer represented as a non-empty array of digits, plus one to the integer.
+        You may assume the integer do not contain any leading zero, except the number 0 itself.
+        The digits are stored such that the most significant digit is at the head of the list.
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        
