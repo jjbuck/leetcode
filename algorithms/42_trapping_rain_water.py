6eb2af95b9f653c3f86cccc01e6279ac9e613bb7
Jonathan <jonathan.j.buck@gmail.com>
2018-01-02 20:28:15 -0800

    Brute force solution to problem #42.

--- algorithms/42_trapping_rain_water.py
+++ algorithms/42_trapping_rain_water.py
@@ -1,6 +1,27 @@
 class Solution:
     def trap(self, height):
         """
+        Given [0,1,0,2,1,0,1,3,2,1,2,1], return 6.
         :type height: List[int]
         :rtype: int
         """
+        water = 0
+        for i in range(len(height)-1):
+            max_left = 0
+            max_right = 0
+            j = i
+            while j >= 0:
+                max_left = max(max_left, height[j])
+                j -= 1
+            j = i
+            while j < len(height):
+                max_right = max(max_right, height[j])
+                j += 1
+            water += min(max_left, max_right) - height[i]
+        return water
+            
+                
+             
+
+            
+assert Solution().trap([0,1,0,2,1,0,1,3,2,1,2,1]) == 6
