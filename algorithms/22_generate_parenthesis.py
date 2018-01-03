fa619a9c1f7a25a8343d317d2678315d1e331a0b
Jonathan <jonathan.j.buck@gmail.com>
2018-01-02 19:35:32 -0800

    Start problem #42.

--- algorithms/22_generate_parenthesis.py
+++ algorithms/22_generate_parenthesis.py
@@ -1,7 +1,21 @@
 class Solution:
-    def generateParenthesis(self, n):
+    def generateParenthesis(self, n, open=0):
         """
         :type n: int
         :rtype: List[str]
         """
+    
+        if n > 0 <= open:
+            return ['(' + p for p in self.generateParenthesis(n-1, open+1)] + [')' + p for p in self.generateParenthesis(n, open-1)]
+        return [')' * open] * (not n)
+                
+            
+            
         
+assert Solution().generateParenthesis(3) == [
+  "((()))",
+  "(()())",
+  "(())()",
+  "()(())",
+  "()()()"
+]
