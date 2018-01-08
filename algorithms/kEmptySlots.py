ab2c018b097843a5b987e2b4c6a882d70e10233f
Jonathan <jonathan.j.buck@gmail.com>
2018-01-07 21:36:25 -0800

    Start kEmptySlots.

---
+++ algorithms/kEmptySlots.py
@@ -0,0 +1,19 @@
+class Solution:
+    def kEmptySlots(self, flowers, k):
+        """
+        :type flowers: List[int]
+        :type k: int
+        :rtype: int
+        """
+        for i in range(len(flowers)-k-1):
+            between = flowers[i+1:i+k+1]
+            flower = flowers[i]
+            next_flower = flowers[i+k+1]
+            if all([check > flower for check in between]) and all([check > next_flower for check in between]):
+                return i+k+1
+        return -1
+            
+        
+assert Solution().kEmptySlots([1,3,2],1) == 2
+assert Solution().kEmptySlots([1,2,3],1) == -1
+assert Solution().kEmptySlots([6,5,8,9,7,1,10,2,3,4],2) == 8
