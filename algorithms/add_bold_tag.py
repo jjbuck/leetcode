66a1cda7ddee4e74434a7ced6e29dc2c352759a0
Jonathan <jonathan.j.buck@gmail.com>
2018-01-12 14:52:46 -0800

    Solve add_bold_tag_in_string

--- algorithms/add_bold_tag.py
+++ algorithms/add_bold_tag.py
@@ -1,10 +1,39 @@
 class Solution:
     def addBoldTag(self, s, dict):
         """
+        Strategy: Build boolean array by traversing array to indicate whether a letter should be bold.
+        Then traverse array again and add bold tags based on boolean
         :type s: str
         :type dict: List[str]
         :rtype: str
         """
+        bold = [False] * len(s)
         for d in dict:
-            if d in s:
+            pos = 0
+            while s.find(d) != -1:
+                ind = s.find(d, pos)
+                if ind == -1:
+                    break
+                bold[ind:(ind+len(d))] = [True for item in bold[ind:(ind+len(d))]]
+                pos += 1
+        # Now traverse array again to add bold tags based on boolean
+        new_str = ''
+        flag = False # used to indicate start or end of bold tag
+        for i in range(len(s)):
+            if bold[i] == True and flag == False: # add new bold tag
+                new_str += '<b>'
+                flag = True
+            if bold[i] == False and flag == True:  # need to close bold tag
+                new_str += '</b>'
+                flag = False
+            new_str += s[i]
+        if flag == True:
+            new_str += '</b>'
+        return new_str
+                
+    
+assert Solution().addBoldTag("abcxyz123",["abc","123"]) == "<b>abc</b>xyz<b>123</b>"
+assert Solution().addBoldTag("aaabbcc",["aaa","aab","bc"]) == "<b>aaabbc</b>c"
+assert Solution().addBoldTag("aaabbcc",["a","b","c"]) == "<b>aaabbcc</b>"
+                
                 
